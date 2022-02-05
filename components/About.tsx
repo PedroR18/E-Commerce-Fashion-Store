e7357ebbb9bd53d43c7d50de212935d3e41ac80c
Pedro Ribeiro <poupas18@gmail.com>
2022-02-05 12:28:52 +0000

    .

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,27 @@
-import { Box, Flex, Image } from '@chakra-ui/react';
+import { Box, Fade, Flex, Image } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
 import AboutGrid from './AboutGrid';
 
 const About = () => {
+  const [reveal, setReveal] = useState(false);
+  const [maskPosition, setMaskPosition] = useState({ x: 0, y: 0 });
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const setCoordinates = (mouse: MouseEvent) => {
+      setMaskPosition({
+        x: mouse.offsetX,
+        y: mouse.offsetY,
+      });
+    };
+
+    ref.current!.addEventListener('mousemove', (mouse) => {
+      window.requestAnimationFrame(() => {
+        setCoordinates(mouse);
+      });
+    });
+  }, []);
+
   return (
     <Flex
       height="100vh"
@@ -12,15 +32,31 @@ const About = () => {
       direction="column"
     >
       <Box height="750px" width="750px" position="relative">
-        <AboutGrid />
-        <Image
-          src="/about-grid-1.jpg"
-          alt="!!PLACEHOLDER!!"
-          filter="grayscale(1)"
+        <Box
           position="absolute"
-          top={0}
-          visibility="hidden"
+          height="100%"
+          width="100%"
+          zIndex={10}
+          onMouseOver={(e) => {
+            setReveal(true);
+          }}
+          onMouseOut={() => setReveal(false)}
+          ref={ref}
         />
+        <AboutGrid />
+        <Fade in={reveal}>
+          <Image
+            src="/about-grid-1.jpg"
+            alt="!!PLACEHOLDER!!"
+            filter="grayscale(1)"
+            position="absolute"
+            top={0}
+            visibility={reveal ? 'visible' : 'hidden'}
+            willChange="transform"
+            clipPath={`circle(20% at ${maskPosition.x}px ${maskPosition.y}px)`}
+            maxHeight="750px"
+          />
+        </Fade>
       </Box>
     </Flex>
   );
