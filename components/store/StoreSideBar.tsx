37faffed9ce79ada1e617b900247bbc43e7b139c
Pedro Ribeiro <poupas18@gmail.com>
2022-02-14 03:54:16 +0000

    Update StoreSideBar.tsx

--- components/store/StoreSideBar.tsx
+++ components/store/StoreSideBar.tsx
@@ -1,9 +1,25 @@
 import { Flex } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const StoreSideBar = () => {
+  const router = useRouter();
   return (
-    <Flex width="300px" height="100vh" bgColor="grey" position="fixed">
+    <Flex
+      width="300px"
+      height="100vh"
+      bgColor="grey"
+      position="fixed"
+      direction="column"
+      gap={5}
+      alignItems="center"
+    >
       <p>StoreSideBar</p>
+      <AiOutlineShopping
+        size={50}
+        onClick={() => router.push('/cart')}
+        cursor="pointer"
+      />
     </Flex>
   );
 };
